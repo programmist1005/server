4c3ad2b868009dee6f35c7aac9f60683e1a03f76
programmist1005 <46303897+programmist1005@users.noreply.github.com>
2019-12-27 21:05:06 +0200

    Add files via upload

---
+++ server.h
@@ -0,0 +1,24 @@
+#ifndef SERVER_H
+# define SERVER_H
+# include <QObject>
+# include <QList>
+
+class QTcpServer;
+class QTcpSocket;
+class ISocketAdapter;
+
+class Server : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Server(int nPort, QObject *parent = nullptr);
+public slots:
+    void on_newConnection();
+    void on_disconnected();
+    void on_message(QString);
+protected:
+    QTcpServer* m_ptcpServer;
+    QList<ISocketAdapter*> m_clients;
+};
+
+#endif // SERVER_H
